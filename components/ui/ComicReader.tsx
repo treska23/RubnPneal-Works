7a22683237d2c366d8c4e4ccebafdb63e3063fe4
treska23 <treska23@hotmail.com>
2025-06-11 14:34:49 +0200

    Simplify comic reader toolbar

--- components/ui/ComicReader.tsx
+++ components/ui/ComicReader.tsx
@@ -1,26 +1,21 @@
 import { Worker, Viewer, SpecialZoomLevel } from '@react-pdf-viewer/core';
-import { defaultLayoutPlugin, DefaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
 import type { ToolbarProps, ToolbarSlot } from '@react-pdf-viewer/toolbar';
 import '@react-pdf-viewer/core/lib/styles/index.css';
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
-import { useMemo } from 'react';
 
 export default function ComicReader() {
-  const layoutPlugin = useMemo(() => {
-    // eslint-disable-next-line prefer-const
-    let plugin: DefaultLayoutPlugin;
-    const renderToolbar = (Toolbar: (props: ToolbarProps) => React.ReactElement) => (
+  const layoutPlugin = defaultLayoutPlugin({
+    renderToolbar: (Toolbar: (props: ToolbarProps) => React.ReactElement) => (
       <Toolbar>
-        {plugin.toolbarPluginInstance.renderDefaultToolbar((slots: ToolbarSlot) => ({
+        {layoutPlugin.toolbarPluginInstance.renderDefaultToolbar((slots: ToolbarSlot) => ({
           ...slots,
           Download: () => null,
           DownloadMenuItem: () => null,
         }))}
       </Toolbar>
-    );
-    plugin = defaultLayoutPlugin({ renderToolbar });
-    return plugin;
-  }, []);
+    ),
+  });
 
   return (
     <div className="relative mx-auto w-[150%] h-screen">
