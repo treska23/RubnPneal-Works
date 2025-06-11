cd0145aa118868378d9f2d028f1e2a4e86b4282a
treska23 <treska23@hotmail.com>
2025-06-11 14:49:36 +0200

    Center comic viewer

--- pages/comic/index.tsx
+++ pages/comic/index.tsx
@@ -3,7 +3,7 @@ import ComicReader from '@/components/ui/ComicReader';
 
 export default function ComicPage() {
   return (
-    <SectionLayout title="Cómic" className="max-w-3xl mx-auto">
+    <SectionLayout title="Cómic" className="w-full mx-auto px-0">
       <h1 className="text-2xl font-medium text-center mb-8">Sumérgete en mi cuento gráfico…</h1>
       <ComicReader />
     </SectionLayout>
