2c968008c52652e927537fa4107a3bc3e1840834
treska23 <treska23@hotmail.com>
2025-06-11 13:08:32 +0200

    Revert "Show PDF file in comic reader"

--- components/ui/ComicReader.tsx
+++ components/ui/ComicReader.tsx
@@ -1,11 +1,7 @@
 export default function ComicReader() {
   return (
-    <div className="w-[150%] h-screen">
-      <iframe
-        src="/Cuando los árboles dejaron de hablar_peq.pdf#toolbar=0"
-        className="w-full h-full border-none m-0 p-0"
-      />
-
+    <div className="w-full h-screen">
+      <iframe src="/Cuando los árboles dejaron de hablar_peq.pdf" className="w-full h-full" />
     </div>
   );
 }
